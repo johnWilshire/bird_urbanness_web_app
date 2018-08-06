9aa7a1c7ee7ac1e383a1a6b5a6448adfa8c27f74
johnWilshire <john.h.wilshire@gmail.com>
2018-08-06 11:05:44 +1000

    updated vun deps

--- src/App.vue
+++ src/App.vue
@@ -9,18 +9,7 @@
     .hero-body
       .container
         phylo-tree
-        .box
-          .content
-            p Abstract
-            p Aliquip non amet minim aliqua dolore veniam. Sunt laborum laborum incididunt aliquip voluptate. Eu officia ex culpa fugiat sunt. Aute voluptate mollit id laborum elit exercitation esse ullamco laboris consectetur magna pariatur voluptate. Ad consequat et veniam eu reprehenderit officia ullamco anim sint cupidatat quis. Ullamco velit ex excepteur officia. Est ea est fugiat pariatur laboris nulla laborum culpa dolor ea do sunt enim consectetur.
-            p Aliquip non amet minim aliqua dolore veniam. Sunt laborum laborum incididunt aliquip voluptate. Eu officia ex culpa fugiat sunt. Aute voluptate mollit id laborum elit exercitation esse ullamco laboris consectetur magna pariatur voluptate. Ad consequat et veniam eu reprehenderit officia ullamco anim sint cupidatat quis. Ullamco velit ex excepteur officia. Est ea est fugiat pariatur laboris nulla laborum culpa dolor ea do sunt enim consectetur.
-            p Aliquip non amet minim aliqua dolore veniam. Sunt laborum laborum incididunt aliquip voluptate. Eu officia ex culpa fugiat sunt. Aute voluptate mollit id laborum elit exercitation esse ullamco laboris consectetur magna pariatur voluptate. Ad consequat et veniam eu reprehenderit officia ullamco anim sint cupidatat quis. Ullamco velit ex excepteur officia. Est ea est fugiat pariatur laboris nulla laborum culpa dolor ea do sunt enim consectetur.
-        .box
-          p Links
-          ul
-            li link 1
-            li link 2
-            li link 3
+ 
 
 </template>
 
